e6b858d1819ab0b433b5172456bfc1a07f21ee44
Bettycnc <betty.canac@hotmail.com>
2025-03-07 09:43:06 +0100

    redirection

--- routes/baby.js
+++ routes/baby.js
@@ -57,7 +57,7 @@ router.post("/babies", async (req, res) => {
 });
 
 //le parent est il associé à un bébé ?
-router.get("/:id", (req, res) => {
+router.get("/redirection/:id", (req, res) => {
   const { id } = req.params;
   //On verifie si le bébé existe dans la base de données
   Baby.findOne({ user_id: id }).then((baby) => {
