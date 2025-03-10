e8ab9de738f477d73e525353d5a07d9d5fbbea30
Kévin <k.schmitt.graph@gmail.com>
2025-03-10 09:30:49 +0100

    Reducer pro

--- routes/pros.js
+++ routes/pros.js
@@ -1,70 +1,78 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-require('../models/connection');
-const userPros = require('../models/userPro');
-const { checkBody } = require('../modules/checkBody');
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
-
+require("../models/connection");
+const userPros = require("../models/userPro");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 // Inscription Pro
-router.post('/signup', (req, res) => {
-    if (!checkBody(req.body, [ 'firstName', 'lastName', 'username', 'password', 'confirmPassword'])) {
-      res.json({ result: false, error: "Tous les champs sont obligatoires" });
-      return;
-    }
+router.post("/signup", (req, res) => {
+  if (
+    !checkBody(req.body, [
+      "firstName",
+      "lastName",
+      "username",
+      "password",
+      "confirmPassword",
+    ])
+  ) {
+    res.json({ result: false, error: "Tous les champs sont obligatoires" });
+    return;
+  }
 
-    if(req.body.password !== req.body.confirmPassword){
-        res.json({result: false, error: "Mots de passe différents"})
-    } else {
-      userPros.findOne({ username: req.body.username }).then(data => {
-      if (data === null) // s'il n'existe pas déjà un nom d'utilisateur égal à celui qui a été rentré
-      
-      {
+  if (req.body.password !== req.body.confirmPassword) {
+    res.json({ result: false, error: "Mots de passe différents" });
+  } else {
+    userPros.findOne({ username: req.body.username }).then((data) => {
+      if (data === null) {
+        // s'il n'existe pas déjà un nom d'utilisateur égal à celui qui a été rentré
+
         const hash = bcrypt.hashSync(req.body.password, 10);
-  
+
         const newUserPro = new userPros({
           firstName: req.body.firstName,
           lastName: req.body.lastName,
           username: req.body.username,
-         // etablissement: req.body.etablissement,
+          // etablissement: req.body.etablissement,
           password: hash,
           token: uid2(32),
         });
-  
-        newUserPro.save().then(newDoc => {
-          res.json({ result: true, token: newDoc.token });
+
+        newUserPro.save().then((newDoc) => {
+          res.json({
+            result: true,
+            token: newDoc.token,
+            username: newDoc.username,
+          });
         });
       } else {
         // User already exists in database
         res.json({ result: false, error: "L'utilisateur existe déjà" });
       }
     });
-}
-
-    
-
-} )
+  }
+});
 
 // Connexion pro
 
-router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['username', 'password'])) {
-    res.json({ result: false, error: 'Tous les champs sont obligatoires' });
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Tous les champs sont obligatoires" });
     return;
   }
 
-  userPros.findOne({ username: req.body.username }).then(data => {
+  userPros.findOne({ username: req.body.username }).then((data) => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, data });
     } else {
-      res.json({ result: false, error: "Le nom d'utilisateur ou le mot de passe est incorrect" });
+      res.json({
+        result: false,
+        error: "Le nom d'utilisateur ou le mot de passe est incorrect",
+      });
     }
   });
 });
-
-
-
 
 module.exports = router;
